914596ecf4cd9bce8ccea46d27d38f2b075a4272
nicolaiandersson <113049347+nicolaiandersson@users.noreply.github.com>
2023-02-06 14:44:14 +0000

    finish igen

---
+++ fix-samples.sh
@@ -0,0 +1,30 @@
+cd samples/animals/
+mv countries/ ../
+
+cd ..
+cd countries/
+mv movies/ ../
+
+cd ..
+cd ..
+cd samples/animals/
+mv Monkies.txt Monkeys.txt
+
+cd ..
+cd movies/
+rm Sex.txt 
+rm And.txt
+rm The.txt
+rm City.txt
+cat > Sex\ And\ The\ City.txt
+
+cd ../animals/
+
+rm Elephant.txt
+rm babies.txt
+cat > Elephant\ babies.txt
+
+
+cd ..
+mv movies/Germany.txt countries/
+mv countries/.apes.txt animals/
